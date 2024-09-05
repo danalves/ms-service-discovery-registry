838fa681f7a125872662ba7de25a0eb63e860f42
Danilo Alves Vieira <vieira.daniloalves@gmail.com>
2024-09-04 21:53:48 -0300

    first commit

---
+++ src/test/java/com/danalves/ms_service_registry/MsServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danalves.ms_service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
